b053b8d0aac074c911428f1d492bbdd8173e29d6
anton <welder.2408@gmail.com>
2022-07-02 01:42:24 +0300

    test commit

--- chariton-api/services/mongoService.js
+++ chariton-api/services/mongoService.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb')
 
 function getAllCharities() {
     return new Promise((resolve, reject) => {
-        // mongodb.find().then(data => resolve(data)).catch(err => reject(err))
+        // mongodb.find().then(data => resolve(data)).catch(err => reject(err))d
     })
 }
 
