b6029e1a33dfc16e8378e6e79664f107d52009e8
Artem Tanyhin <magic.cat.775@gmail.com>
2022-07-03 03:40:58 -0400

    Merge branch 'backend-mongo' of https://github.com/devils2ndself/kherson-underground into backend-ton

--- chariton-api/index.js
+++ chariton-api/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
+
 const mongoService = require('./services/mongoService')
 const TonService = require('./services/tonService')
+const convert = require('./convert/convert');
+const previousRides = require('./models/previousRides');
+const WsPay = require('./models/payment');
 
 const port = process.env.PORT || 8080;
 
@@ -14,29 +18,107 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: 'API is working properly.'});
+    res.json({
+        message: 'API is working properly.'
+    });
 })
 
-app.get('/api/balance', (req, res) => {
-    tonService.getBalance()
-        .then(balance => {
-            res.json(balance)
-        })
-        .catch(err => {
-            res.status(500).json({message: err})
-        })
+app.get('/api/rentals', (req, res) => {
+    mongoService.getAllRentals().then(async (data) => {
+        const exRate = await convert.convert();
+        data.forEach(element => {
+            element.tariff = (element.tariff * exRate).toFixed(2);
+        });
+        res.json(data);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+app.get('/api/previousRides', (req, res) => {
+    mongoService.getAllPreviousRides().then((data) => {
+
+        res.json(data);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+// пусть будет
+// app.get('/api/exchangeRate', (req, res) => {
+//     convert.convert().then((data) => {
+//         res.json(data);
+//     }).catch(err => {
+//         console.log(err);
+//         res.status.json(err);
+//     })
+// })
+
+app.get('/api/rentals/:id', async (req, res) => {
+    const exRate = await convert.convert();
+    mongoService.getRentalById(req.params.id).then(async (data) => {
+        data.tariff = (data.tariff * exRate).toFixed(2);
+        res.json(data);
+    })
+})
+
+app.post('/api/start', async (req, res) => { //req.body.id
+
+    const currRental = await mongoService.getRentalById(req.body.id);
+    const exRate = await convert.convert();
+    var amount = (currRental.tariff * exRate).toFixed(2);
+    WsPay(amount, 'start');
+
+    res.statusCode = 201;
+    res.setHeader('Content-Type', 'text/json');
+    res.end();
+
+})
+
+app.post('/api/stop', async (req, res) => { //req.body.id
+    console.log("Stoped");
+    WsPay(null, 'stop');
+
+    const currRental = await mongoService.getRentalById(req.body.id);
+    const exRate = await convert.convert();
+    var amount = (currRental.tariff * exRate).toFixed(2);
+
+    var data = new previousRides(null, null, null, null, null);
+    // 
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    //
+    var tot = Number(amount);
+    tot *= (req.body.time / 60);
+    data.startDate = today;
+    data.time = req.body.time;
+    data.totalAmount = tot.toFixed(2);
+
+    data.type = currRental.type;
+    data.tariff = currRental.tariff;
+    await mongoService.addPreviousRide(data);
+
+    res.statusCode = 201;
+    res.setHeader('Content-Type', 'text/json');
+    res.end();
 })
 
 mongoService.connectMongo()
     .then(()=> {
-        // tonService.initWallets()
-        //     .then(() => {
-        //         app.listen(port, () => {
-        //             console.log('App listening on port:', port)
-        //         })
-        //     })
-        //     .catch(err => console.log('Error initializing wallets!'))
+        tonService.initWallets()
+            .then(() => {
+                app.listen(port, () => {
+                    console.log('App listening on port:', port)
+                })
+            })
+            .catch(err => console.log('Error initializing wallets!'))
     })
     .catch(err => {
-        console.log('Error connecting to the DB!')
+        console.log(err)
     })
--- chariton-api/services/mongoService.js
+++ chariton-api/services/mongoService.js
@@ -1,20 +1,75 @@
-const mongodb = require('mongodb');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const Rentals = require("../models/rentals");
+const PreviousRides = require("../models/previousRides");
+require('dotenv').config({
+    path: `${__dirname}/../.env`
+});
 
-function connectMongo() {
+const conf = {
+    dbUrl: process.env.DB_CON_STR,
+    connectOptions: {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+};
+
+async function connectMongo() {
+    try {
+        await mongoose.connect(conf.dbUrl, conf.connectOptions);
+        console.log("DB listening at " + conf.dbUrl);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getAllRentals() {
     return new Promise((resolve, reject) => {
-        // mongodb.connect(process.env.DB_CON_STR).then()
-        resolve()
+        Rentals.find().then(data => resolve(data)).catch(err => reject(err));
     })
 }
 
-function getAllCharities() {
+async function getRentalById(params) {
+
+
+    try {
+        return await Rentals.findById(params);
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+
+}
+// async function getRentalByName(params) {
+
+//     try {
+//         return await Rentals.findOne({
+//             type: params
+//         });
+//     } catch (err) {
+//         console.log(err);
+//         return null;
+//     }
+
+// }
+
+function addPreviousRide(params) {
+
     return new Promise((resolve, reject) => {
-        // mongodb.find().then(data => resolve(data)).catch(err => reject(err))
-    })
+        PreviousRides(params).save().then();
+        resolve();
+    });
+
 }
 
+function getAllPreviousRides() {
+    return new Promise((resolve, reject) => {
+        PreviousRides.find().then(data => resolve(data)).catch(err => reject(err));
+    })
+}
 module.exports = {
     connectMongo,
-    getAllCharities
+    getAllRentals,
+    getRentalById,
+    getAllPreviousRides,
+    addPreviousRide
 }
